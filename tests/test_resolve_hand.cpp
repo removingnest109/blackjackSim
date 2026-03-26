0b7761f3cc3bfb86d71e50bb3df082a748b708bf
Ethan Saenger <ethan@saenger5.com>
2026-03-26 18:46:59 -0500

    clang-format tests

--- tests/test_resolve_hand.cpp
+++ tests/test_resolve_hand.cpp
@@ -4,67 +4,67 @@
 #include "gtest/gtest.h"
 
 TEST(ResolveHand, ResolvesDrawCorrectly) {
-    Hand hp, hd;
-    Stats s;
-    hp.cardCount = 2;
-    hp.value = 20;
-    hd.cardCount = 2;
-    hd.value = 20;
-    resolveHand(hp, hd, s);
-    EXPECT_TRUE(s.draw > 0);
+  Hand hp, hd;
+  Stats s;
+  hp.cardCount = 2;
+  hp.value = 20;
+  hd.cardCount = 2;
+  hd.value = 20;
+  resolveHand(hp, hd, s);
+  EXPECT_TRUE(s.draw > 0);
 }
 
 TEST(ResolveHand, ResolvesPlayerWinCorrectly) {
-    Hand hp, hd;
-    Stats s;
-    hp.cardCount = 3;
-    hp.value = 20;
-    hd.cardCount = 2;
-    hd.value = 19;
-    resolveHand(hp, hd, s);
-    EXPECT_TRUE(s.playerWins > 0);
+  Hand hp, hd;
+  Stats s;
+  hp.cardCount = 3;
+  hp.value = 20;
+  hd.cardCount = 2;
+  hd.value = 19;
+  resolveHand(hp, hd, s);
+  EXPECT_TRUE(s.playerWins > 0);
 }
 
 TEST(ResolveHand, ResolvesPlayerBustCorrectly) {
-    Hand hp, hd;
-    Stats s;
-    hp.cardCount = 3;
-    hp.value = 22;
-    hd.cardCount = 2;
-    hd.value = 20;
-    resolveHand(hp, hd, s);
-    EXPECT_TRUE(s.dealerWins > 0);
+  Hand hp, hd;
+  Stats s;
+  hp.cardCount = 3;
+  hp.value = 22;
+  hd.cardCount = 2;
+  hd.value = 20;
+  resolveHand(hp, hd, s);
+  EXPECT_TRUE(s.dealerWins > 0);
 }
 
 TEST(ResolveHand, ResolvesBothBustCorrectly) {
-    Hand hp, hd;
-    Stats s;
-    hp.cardCount = 3;
-    hp.value = 22;
-    hd.cardCount = 3;
-    hd.value = 22;
-    resolveHand(hp, hd, s);
-    EXPECT_TRUE(s.dealerWins > 0);
+  Hand hp, hd;
+  Stats s;
+  hp.cardCount = 3;
+  hp.value = 22;
+  hd.cardCount = 3;
+  hd.value = 22;
+  resolveHand(hp, hd, s);
+  EXPECT_TRUE(s.dealerWins > 0);
 }
 
 TEST(ResolveHand, ResolvesDealerWinCorrectly) {
-    Hand hp, hd;
-    Stats s;
-    hp.cardCount = 3;
-    hp.value = 19;
-    hd.cardCount = 3;
-    hd.value = 20;
-    resolveHand(hp, hd, s);
-    EXPECT_TRUE(s.dealerWins > 0);
+  Hand hp, hd;
+  Stats s;
+  hp.cardCount = 3;
+  hp.value = 19;
+  hd.cardCount = 3;
+  hd.value = 20;
+  resolveHand(hp, hd, s);
+  EXPECT_TRUE(s.dealerWins > 0);
 }
 
 TEST(ResolveHand, ResolvesDealerBustCorrectly) {
-    Hand hp, hd;
-    Stats s;
-    hp.cardCount = 2;
-    hp.value = 20;
-    hd.cardCount = 3;
-    hd.value = 22;
-    resolveHand(hp, hd, s);
-    EXPECT_TRUE(s.playerWins > 0);
+  Hand hp, hd;
+  Stats s;
+  hp.cardCount = 2;
+  hp.value = 20;
+  hd.cardCount = 3;
+  hd.value = 22;
+  resolveHand(hp, hd, s);
+  EXPECT_TRUE(s.playerWins > 0);
 }
