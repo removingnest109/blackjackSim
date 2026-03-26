3a896a44b838451c2cf8b99628e741e03ab4c30c
Ethan Saenger <ethan@saenger5.com>
2026-03-26 18:46:59 -0500

    clang-format tests

--- tests/test_blackjack.cpp
+++ tests/test_blackjack.cpp
@@ -3,42 +3,44 @@
 #include <gtest/gtest.h>
 
 TEST(Blackjack, DetectsNaturalBlackjackOnly) {
-    Hand h;
-    h.cardCount = 2;
-    h.value = 21;
-    h.splitAces = false;
-    EXPECT_TRUE(isBlackjack(h));
+  Hand h;
+  h.cardCount = 2;
+  h.value = 21;
+  h.splitAces = false;
+  EXPECT_TRUE(isBlackjack(h));
 
-    h.splitAces = true;
-    EXPECT_FALSE(isBlackjack(h));
+  h.splitAces = true;
+  EXPECT_FALSE(isBlackjack(h));
 }
 
 TEST(Blackjack, DetectsBothBlackjacksWithBetReturned) {
-    Hand hp, hd;
-    Stats s;
-    hp.cardCount = 2;
-    hp.value = 21;
-    hd.cardCount = 2;
-    hd.value = 21;
-    EXPECT_TRUE(detectBlackjacks(hp, hd, 100, s) && s.draw > 0 && s.bank == 100);
+  Hand hp, hd;
+  Stats s;
+  hp.cardCount = 2;
+  hp.value = 21;
+  hd.cardCount = 2;
+  hd.value = 21;
+  EXPECT_TRUE(detectBlackjacks(hp, hd, 100, s) && s.draw > 0 && s.bank == 100);
 }
 
 TEST(Blackjack, DetectsDealerBlackjack) {
-    Hand hp, hd;
-    Stats s;
-    hp.cardCount = 2;
-    hp.value = 20;
-    hd.cardCount = 2;
-    hd.value = 21;
-    EXPECT_TRUE(detectBlackjacks(hp, hd, 100, s) && s.dealerBlackjacks > 0 && s.bank == 0);
+  Hand hp, hd;
+  Stats s;
+  hp.cardCount = 2;
+  hp.value = 20;
+  hd.cardCount = 2;
+  hd.value = 21;
+  EXPECT_TRUE(detectBlackjacks(hp, hd, 100, s) && s.dealerBlackjacks > 0 &&
+              s.bank == 0);
 }
 
 TEST(Blackjack, DetectsPlayerBlackjackWithPayout) {
-    Hand hp, hd;
-    Stats s;
-    hp.cardCount = 2;
-    hp.value = 21;
-    hd.cardCount = 2;
-    hd.value = 20;
-    EXPECT_TRUE(detectBlackjacks(hp, hd, 100, s) && s.playerBlackjacks > 0 && s.bank == 250);
+  Hand hp, hd;
+  Stats s;
+  hp.cardCount = 2;
+  hp.value = 21;
+  hd.cardCount = 2;
+  hd.value = 20;
+  EXPECT_TRUE(detectBlackjacks(hp, hd, 100, s) && s.playerBlackjacks > 0 &&
+              s.bank == 250);
 }
