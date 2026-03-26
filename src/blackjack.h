330f9d03e673b10a0c1916fd4f835c7ffadbd915
Ethan Saenger <ethan@saenger5.com>
2026-03-26 14:35:49 -0500

    format and move shuffleIfNeeded outside of dealInitialCards

--- src/blackjack.h
+++ src/blackjack.h
@@ -4,13 +4,14 @@
 #include "stats.h"
 #include <random>
 
-void drawCard(std::vector<int> &deck, Hand &hand, const bool &visible, Stats &stats);
+void drawCard(std::vector<int> &deck, Hand &hand, const bool &visible,
+              Stats &stats);
 void initDeck(std::vector<int> &deck);
 void shuffleDeck(std::vector<int> &deck, std::mt19937 &rng, Stats &stats);
 void resetHand(Hand &hand, const int64_t &bet = 0);
 void shuffleIfNeeded(std::vector<int> &deck, std::mt19937 &rng, Stats &stats);
-void dealInitialCards(std::vector<int> &deck, Hand &handPlayer, Hand &handDealer,
-                      std::mt19937 &rng, const int64_t &bet, Stats &stats);
+void dealInitialCards(std::vector<int> &deck, Hand &handPlayer,
+                      Hand &handDealer, const int64_t &bet, Stats &stats);
 Hand makeHand(const int64_t &bet);
 Hand split(std::vector<int> &deck, Hand &originalHand, Stats &stats);
 void doubleDown(std::vector<int> &deck, Hand &hand, Stats &stats);
--- src/interactive.h
+++ src/interactive.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <vector>
 #include "model.h"
 #include "stats.h"
+#include <vector>
 
 void interactiveHand(std::vector<int> &deck, Hand hands[], int &handCount,
                      const Hand &dealer, Stats &stats);
